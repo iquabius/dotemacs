fddcebf99ce511a4682938ecb03c22133c4e4ba2
Josias Duarte Busiquia <iquabius@gmail.com>
2016-12-27 11:47:58 -0300

    Change default theme from Cyberpunk to Zenburn

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7b4d9b8a6ada8e24ac9eecd057093b0572d7008dbd912328231d0cada776065a" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" default)))
+ '(git-gutter:modified-sign "●")
  '(package-selected-packages
    (quote
     (helm-swoop helm-ag helm autopair company cyberpunk-theme diminish nyan-mode solarized-theme use-package))))
