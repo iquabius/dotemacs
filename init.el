794088eb0d8fb74ff5dc71fab205d4c6497b12fb
Josias Duarte Busiquia <iquabius@gmail.com>
2017-03-19 14:54:19 -0400

    Setup config for running daemon and emacsclient

--- init.el
+++ init.el
@@ -1,9 +1,8 @@
 ;;; Begin initialization
 ;; Turn off mouse interface early in startup to avoid momentary display
-(when window-system
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
